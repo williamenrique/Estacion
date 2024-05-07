469e87ca4bda168b88c9b1c026630e1fda958091
williamenrique <william21enrique@gmail.com>
2024-05-06 20:57:51 -0400

    estructura simple

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Estacion
+# Dashboard-LTE-B4.5
+# Bootstrap 4.5.3
+# FontAwesome 14.5
+# Plantilla Dashoard LTE
+# panelsystem.lat
---
+++ system/app/Views/Modules/footer.php
@@ -0,0 +1,5 @@
+    <script>const base_url = "<?= base_url()?>";</script>
+    <script src="<?= JS.$data['page_functions']?>"></script>
+</body>
+
+</html>

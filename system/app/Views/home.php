5b01a90140c3bf084749c09f1648a3ca93d8c6e2
williamenrique <william21enrique@gmail.com>
2024-05-07 15:50:41 -0400

    login e iniciar

--- system/app/Views/home.php
+++ system/app/Views/home.php
@@ -3,7 +3,7 @@
 	<div class="contenedor">
 		<h2 clas="">Framework v1.0</h2>
 		<h3>Realizado con PHP MVC</h3>
-		<p><a href="home">Panel de Control prueba</a></p>
+		<p><a href="<?= base_url()?>/logout">salir</a></p>
 	</div>
 
 <?= footer($data)?>

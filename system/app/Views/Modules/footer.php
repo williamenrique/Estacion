bbce717c27b77c8c49bd0802ba934e40d697dc6b
williamenrique <william21enrique@gmail.com>
2024-05-13 11:10:21 -0400

    footer

--- system/app/Views/Modules/footer.php
+++ system/app/Views/Modules/footer.php
@@ -8,10 +8,11 @@
     <footer class="main-footer">
     	<!-- To the right -->
     	<div class="float-right d-none d-sm-inline">
-    		Anything you want
+    		<!-- Anything you want -->
     	</div>
     	<!-- Default to the left -->
-    	<strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    	<strong>Copyright &copy; 2024 <a href="<?= base_url()?>">Administracion</a>.</strong> Todos los derechos
+    	reservados
     </footer>
     </div>
     <!-- ./wrapper -->
